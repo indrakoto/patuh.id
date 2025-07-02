72ed16205c65703043c0781c3963bd1fd32bd81b
Koto <indrakoto@gmail.com>
2025-07-02 09:00:16 +0700

    update Officer Panel

--- src/app/Providers/Filament/OfficerPanelProvider.php
+++ src/app/Providers/Filament/OfficerPanelProvider.php
@@ -17,6 +17,8 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\View\PanelsRenderHook;
+use Illuminate\Support\Facades\Blade;
 
 class OfficerPanelProvider extends PanelProvider
 {
@@ -28,8 +30,14 @@ class OfficerPanelProvider extends PanelProvider
             ->path('officer')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' =>  Color::hex('#3c5589'), // Warna custom '#3c5589',
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
+            ->darkMode(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -38,7 +46,7 @@ class OfficerPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                //Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -53,6 +61,19 @@ class OfficerPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->renderHook(PanelsRenderHook::SIMPLE_PAGE_END, function () {
+                return Blade::render('
+                    <p class="mt-2 text-center text-sm text-gray-500 dark:text-gray-400" >
+                    <a href="/" style="fi-link group/link relative inline-flex items-center justify-center outline-none fi-size-md fi-link-size-md gap-1.5 fi-color-custom fi-color-primary fi-ac-action fi-ac-link-action">
+                    <span class="font-semibold text-sm text-custom-600 dark:text-custom-400 group-hover/link:underline group-focus-visible/link:underline" style="--c-400:var(--primary-400);--c-600:var(--primary-600);">
+                        {{ $text }}
+                    </span>  
+                    </a>
+                    </p>
+                    ', [
+                    'text' => 'Ke Beranda',
+                ]);
+            });
     }
 }
