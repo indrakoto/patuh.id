754227ebe107125a8d6b1925f68b74e83968f6c3
Koto <indrakoto@gmail.com>
2025-07-02 23:00:24 +0700

    update auth controller

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended('/forum')->with('success', 'Login berhasil!');
+            return redirect()->intended('/')->with('success', 'Login berhasil!');
         }
 
         return back()->withErrors([
