f0601070b308aca69bde792b1a7601db7df34f2b
Koto <indrakoto@gmail.com>
2025-07-04 15:11:24 +0700

    menambah news

--- src/resources/views/news/show.blade.php
+++ src/resources/views/news/show.blade.php
@@ -33,47 +33,47 @@
                     <div class="knowledges-knowledge-lainnya">
                         <h3>Berita Lainnya</h3>
                         
-<div class="">
-    @foreach($relatedNews as $related)
-    <div class="knowledge-lainnya-item mt-4 mb-3 align-items-center">
-        <!-- Kolom untuk thumbnail -->
-        <div class="col-md-12">
-            @php
-                $thumbnail = $related->thumbnail 
-                    ? asset('articles/thumbnails/' . $related->thumbnail)
-                    : asset('img/default.png');
-            @endphp
-            <img src="{{ $thumbnail }}" class="img-fluid rounded" alt="{{ $related->title }}">
-        </div>
-        
-        <!-- Kolom untuk konten -->
-        <div class="col-md-12">
-            <h3 class="mt-3 mb-3">
-                
-                <a href="{{ route('knowledge.show', ['article_slug' => $related->slug, 'id' => $related->id]) }}">
-                    {{ $related->title }}
-                </a>
-   
-            </h3>
-            <div class="trainer d-flex justify-content-between align-items-center">
-                <div class="trainer-profile d-flex align-items-center">
-                    <i class="bi bi-calendar2-event me-1"></i>
-                    {{ $related->created_at->format('d M Y') }}
-                </div>
-                <div class="trainer-rank d-flex align-items-center">
-                    <i class="bi bi-eye me-1"></i>
-                    {{ $related->views }}
-                    
-                          &nbsp;&nbsp;
-                          <i class="bi bi-people user-icon"></i>&nbsp;0
-                          &nbsp;&nbsp;
-                          <i class="bi bi-star-fill start-icon" style="color:rgb(233, 187, 89);"></i>&nbsp;0
-                </div>
-            </div>
-        </div>
-    </div>
-    @endforeach
-</div>
+                        <div class="">
+                            @foreach($relatedNews as $related)
+                            <div class="knowledge-lainnya-item mt-4 mb-3 align-items-center">
+                                <!-- Kolom untuk thumbnail -->
+                                <div class="col-md-12">
+                                    @php
+                                        $thumbnail = $related->thumbnail 
+                                            ? asset('articles/thumbnails/' . $related->thumbnail)
+                                            : asset('img/default.png');
+                                    @endphp
+                                    <img src="{{ $thumbnail }}" class="img-fluid rounded" alt="{{ $related->title }}">
+                                </div>
+                                
+                                <!-- Kolom untuk konten -->
+                                <div class="col-md-12">
+                                    <h3 class="mt-3 mb-3">
+                                        
+                                        <a href="{{ route('knowledge.show', ['article_slug' => $related->slug, 'id' => $related->id]) }}">
+                                            {{ $related->title }}
+                                        </a>
+                        
+                                    </h3>
+                                    <div class="trainer d-flex justify-content-between align-items-center">
+                                        <div class="trainer-profile d-flex align-items-center">
+                                            <i class="bi bi-calendar2-event me-1"></i>
+                                            {{ $related->created_at->format('d M Y') }}
+                                        </div>
+                                        <div class="trainer-rank d-flex align-items-center">
+                                            <i class="bi bi-eye me-1"></i>
+                                            {{ $related->views }}
+                                            
+                                                &nbsp;&nbsp;
+                                                <i class="bi bi-people user-icon"></i>&nbsp;0
+                                                &nbsp;&nbsp;
+                                                <i class="bi bi-star-fill start-icon" style="color:rgb(233, 187, 89);"></i>&nbsp;0
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            @endforeach
+                        </div>
 
                     </div>
                 </div>
